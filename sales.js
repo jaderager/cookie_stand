c8d9526bbf594fb7bdbb816816d81748c82fe4c1
jaderager <cameronrager@gmail.com>
2017-02-13 17:55:03 -0800

    add .genHourlySales() method which stores results in .salesArray property

--- sales.js
+++ sales.js
@@ -4,40 +4,45 @@ var firstAndPike = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerCust: 6.3,
+  salesArray: []
 };
 
 var seaTacAirport = {
   location: 'SeaTac Airport',
   minCustomers: 3,
   maxCustomers: 24,
-  avgCookiesPerCust: 1.2
+  avgCookiesPerCust: 1.2,
+  salesArray: []
 };
 
 var seattleCenter = {
   location: 'Seattle Center',
   minCustomers: 11,
   maxCustomers: 38,
-  avgCookiesPerCust: 3.7
+  avgCookiesPerCust: 3.7,
+  salesArray: []
 };
 
 var capitolHill = {
   location: 'Capitol Hill',
   minCustomers: 20,
   maxCustomers: 38,
-  avgCookiesPerCust: 2.3
+  avgCookiesPerCust: 2.3,
+  salesArray: []
 };
 
 var alki = {
   location: 'Alki',
   minCustomers: 2,
   maxCustomers: 16,
-  avgCookiesPerCust: 4.6
+  avgCookiesPerCust: 4.6,
+  salesArray: []
 };
 
 function genRandomCust() {
   var min = Math.ceil(this.minCustomers);
   var max = Math.floor(this.maxCustomers);
-  return Math.floor(Math.random() * (min + 1 - max) + min);
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
 firstAndPike.genRandomCust = genRandomCust;
@@ -47,7 +52,7 @@ capitolHill.genRandomCust = genRandomCust;
 alki.genRandomCust = genRandomCust;
 
 function cookiesPerHour() {
-  return this.genRandomCust * this.avgCookiesPerCust;
+  return this.genRandomCust() * this.avgCookiesPerCust;
 }
 
 firstAndPike.cookiesPerHour = cookiesPerHour;
@@ -55,6 +60,43 @@ seaTacAirport.cookiesPerHour = cookiesPerHour;
 seattleCenter.cookiesPerHour = cookiesPerHour;
 capitolHill.cookiesPerHour = cookiesPerHour;
 alki.cookiesPerHour = cookiesPerHour;
+
+function genHourlySales() {
+
+  for (var i = 6; i <= 20; i++) {
+    var amPmFlag; //12hr am/pm bit
+    var twelveHourTime; //12hr hour value
+    var workingString = ''; //temp string
+
+    if (i < 12) { //if before noon
+      amPmFlag = 0;
+      twelveHourTime = i; 
+    } else if (i === 12) { //if noon
+      amPmFlag = 1;
+      twelveHourTime = 12; //naturally. value of 0 for below calc would not work.
+    } else {
+      amPmFlag = 1;
+      twelveHourTime = i - 12; //convert to 12hr. does not work at noon
+    }
+
+    workingString += twelveHourTime; //6 ...
+    if (!amPmFlag) {
+      workingString += 'am: '; //6am: ...
+    } else if (amPmFlag) {
+      workingString += 'pm: '; //6pm: ...
+    }
+
+    workingString += this.cookiesPerHour() + ' cookies'; //6xm: __ cookies 
+
+    this.salesArray.push(workingString);
+  }
+}
+
+firstAndPike.genHourlySales = genHourlySales;
+seaTacAirport.genHourlySales = genHourlySales;
+seattleCenter.genHourlySales = genHourlySales;
+capitolHill.genHourlySales = genHourlySales;
+alki.genHourlySales = genHourlySales;
 
 document.onload = function() {
     //create for loop to generate list elements
